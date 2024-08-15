1951b1c1df7c4ec3eb27ff9abef1b8eaa232c1a8
Daniel Mera <89852311+danmera1980@users.noreply.github.com>
2024-08-14 21:00:56 -0500

    Cambio en url

--- client/src/enviroments/development.enviroment.ts
+++ client/src/enviroments/development.enviroment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     baseUrl: 'http://localhost:4200',
     envVar: {
-        API_URL:'http://localhost:8000/api'
+        API_URL:'https://unibrica.onrender.com/api'
     }
   };
